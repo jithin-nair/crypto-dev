907f5c1acfa30913ead6dda68b45b3af9a3fedd0
jithin <jithin@jithin-pc>
2018-02-21 22:40:16 +0530

    Modified Controller and Service
    Get request and service call

--- tychexplore/src/main/java/com/tychecash/tychexplore/controller/TycheRestController.java
+++ tychexplore/src/main/java/com/tychecash/tychexplore/controller/TycheRestController.java
@@ -5,7 +5,15 @@
  */
 package com.tychecash.tychexplore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tychecash.tychexplore.model.response.BaseResponse;
+import com.tychecash.tychexplore.model.response.BlockResponse;
+import com.tychecash.tychexplore.service.TycheExploreService;
 
 /**
  *
@@ -14,6 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TycheRestController {
     
+    @Autowired
+    private TycheExploreService tycheExploreService;
     
+    @GetMapping("/getRecentBlocks")
+	public BaseResponse getRecentBlocks() {
+		BlockResponse lastBlockResponse = tycheExploreService.getLastBlockResponse();
+		BaseResponse baseResponse = new BaseResponse();
+		baseResponse.setResponseBody(lastBlockResponse);
+		return baseResponse;
+	}
     
 }
--- tychexplore/src/main/java/com/tychecash/tychexplore/service/impl/TycheExploreServiceImpl.java
+++ tychexplore/src/main/java/com/tychecash/tychexplore/service/impl/TycheExploreServiceImpl.java
@@ -5,6 +5,9 @@
  */
 package com.tychecash.tychexplore.service.impl;
 
+import org.springframework.stereotype.Component;
+
+import com.tychecash.tychexplore.model.request.BlockRequest;
 import com.tychecash.tychexplore.model.response.BlockResponse;
 import com.tychecash.tychexplore.service.TycheExploreService;
 
@@ -12,26 +15,35 @@ import com.tychecash.tychexplore.service.TycheExploreService;
  *
  * @author jithin
  */
+@Component
 public class TycheExploreServiceImpl implements TycheExploreService {
 
-    @Override
-    public BlockResponse getBlockResponseByHeight(String height) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public BlockResponse getBlockResponseByHeight(String height) {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
 
-    @Override
-    public BlockResponse getBlockResponseByHash(String hash) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public BlockResponse getBlockResponseByHash(String hash) {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
 
-    @Override
-    public BlockResponse getLastBlockResponse() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public BlockResponse getLastBlockResponse() {
+		BlockResponse blockResponse = null;
+		BlockRequest blockRequest = new BlockRequest();
+		blockRequest.setId("self");
+		blockRequest.setJsonrpc("2.0");
+		blockRequest.setMethod("getlastblockheader");
+		return blockResponse;
+	}
 
-    @Override
-    public BlockResponse getLastNBlockResponseFromHeight(String height, Integer size) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+	@Override
+	public BlockResponse getLastNBlockResponseFromHeight(String height, Integer size) {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
+
 }
